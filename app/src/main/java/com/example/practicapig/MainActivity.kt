42bbd350918f6e711479562a1e391665fb31328e
SergioLilo <sergioponceplaza@gmail.com>
2024-10-22 13:55:51 +0200

    Si

--- app/src/main/java/com/example/practicapig/MainActivity.kt
+++ app/src/main/java/com/example/practicapig/MainActivity.kt
@@ -7,9 +7,10 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.practicapig.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +18,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.botonTirar.visibility=View.GONE
-        var salir:Boolean=false
+        binding.puntJUG.visibility=View.GONE
+        binding.TurnJug.visibility=View.GONE
+        binding.pasarID.visibility=View.GONE
+        binding.rondaID.visibility=View.GONE
+        binding.clasificacionID.visibility=View.GONE
+        binding.IDganador.visibility=View.GONE
         var texto = ""
         var num: Int=0
-        val jugadores = ArrayList<Jugador>()
+        var numTurno=0
+        var jugadores = ArrayList<Jugador>()
+        var rondas:Int=4
 
         binding.eleccionNum.setOnCheckedChangeListener { _, id ->
             val radioButton = findViewById<RadioButton>(id)
@@ -30,20 +38,82 @@ class MainActivity : AppCompatActivity() {
         binding.salirID.setOnClickListener {
             num = pulsarBoton(texto, num)
             for (i in 1..num) {
-                jugadores.add(Jugador())
+                jugadores.add(Jugador("Jugador "+i))
+            }
+            var dado:Int=0
+            var numtirado=0
+            var turno=1
+            binding.TurnJug.text= "Turno del "+jugadores.get(0).nombre
+            binding.rondaID.visibility=View.VISIBLE
+            binding.clasificacionID.visibility=View.VISIBLE
+            binding.rondaID.text= "RONDA: "+turno
+
+                binding.botonTirar.setOnClickListener {
+
+                dado=jugadores[numTurno].tirar()
+                binding.puntJUG.text="EL JUGADOR "+(numTurno+1)+" HA SACADO "+dado
+                numtirado++
+
+                if (numtirado!=0)binding.pasarID.visibility=View.VISIBLE
+                if (dado==1) {
+                    numTurno++
+                    if (numTurno==num){
+                        numTurno=0
+                        turno++
+                        binding.rondaID.text= "RONDA: "+turno
+                    }
+                    binding.pasarID.visibility=View.GONE
+                    numtirado=0
+                }
+                binding.pasarID.setOnClickListener{
+                    numTurno++
+                    if (numTurno==num) {
+                        numTurno=0
+                        turno++
+                        binding.rondaID.text= "RONDA: "+turno
+                    }
+                    binding.pasarID.visibility=View.GONE
+                    binding.TurnJug.text= "Turno del Jugador "+(numTurno+1)
+                    numtirado=0
+                    clasificacion(binding,jugadores);
+
+                    if (turno==rondas+1){
+                        ganador(jugadores)
+
+
+                    }
+                }
+
+                clasificacion(binding, jugadores)
+                    if (turno==rondas+1){
+                        ganador(jugadores)
+                    }
+
             }
 
 
-            binding.botonTirar.setOnClickListener {
 
 
-            }
 
 
         }
 
 
 
+
+    }
+
+    private fun ganador(jugadores: ArrayList<Jugador>) {
+        binding.pasarID.visibility = View.GONE
+        binding.TurnJug.visibility = View.GONE
+        binding.botonTirar.visibility = View.GONE
+        binding.rondaID.visibility = View.GONE
+        binding.puntJUG.visibility = View.GONE
+        clasificacion(binding, jugadores)
+        binding.IDganador.visibility = View.VISIBLE
+        //jugadores.sortedBy { it.puntuacion }
+        jugadores.sortByDescending { it.puntuacion }
+        binding.IDganador.text = "EL GANADOR ES: " + jugadores.get(0).nombre
 
     }
 
@@ -60,6 +130,8 @@ class MainActivity : AppCompatActivity() {
                 binding.textView.visibility = View.GONE
 
                 binding.botonTirar.visibility=View.VISIBLE
+                binding.puntJUG.visibility=View.VISIBLE
+                binding.TurnJug.visibility=View.VISIBLE
                 Toast.makeText(this, "Número seleccionado: " + num1, Toast.LENGTH_SHORT).show()
 
             }
@@ -69,9 +141,40 @@ class MainActivity : AppCompatActivity() {
         return num1
     }
 
-    class Jugador{
+    private fun clasificacion(binding: ActivityMainBinding, jugadores: ArrayList<Jugador>){
+        var mensaje:String=""
+        for (i in 0 .. jugadores.size-1){
+
+            mensaje=mensaje.plus("\n JUGADOR "+(i+1)+": "+jugadores[i].puntuacion)
+
+        }
+        binding.clasificacionID.text=mensaje
+
+    }
+    private fun ganador(binding: ActivityMainBinding, jugadores: ArrayList<Jugador>){
+
+
+    }
+
+    class Jugador(s: String) {
 
          var puntuacion:Int=0
+         var nombre:String=s
+
+        public fun tirar(): Int {
+        var dado=Random.nextInt(1,7)
+            when(dado) {
+                1 -> puntuacion=0
+                2 -> puntuacion=puntuacion+2
+                3 -> puntuacion=puntuacion+3
+                4 -> puntuacion=puntuacion+4
+                5 -> puntuacion=puntuacion+5
+                6 -> puntuacion=puntuacion+6
+            }
+          return dado
+
+        }
+
     }
 
 }
